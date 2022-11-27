0f09fb9daac20b9124b255e8a25a681ed5e3cefe
Victor Semenovich <victor.semenovich@silkailines.com>
2022-11-27 21:11:54 +0300

    push notifications ios config

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:operators/firebase_options.dart';
 import 'package:operators/src/app.dart';
@@ -8,5 +9,9 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  debugPrint('fcmToken: $fcmToken');
+
   runApp(OperatorsApp());
 }

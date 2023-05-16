289d352da147e4fd2b1cce5bf9d18b65774e7538
Victor Semenovich <victor.semenovich@gmail.com>
2023-05-16 11:39:30 +0300

    improved event json parsing

--- lib/generated/src/data/remote/dto/event.g.dart
+++ lib/generated/src/data/remote/dto/event.g.dart
@@ -6,29 +6,15 @@ part of '../../../../../src/data/remote/dto/event.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-_$_EventDTO _$$_EventDTOFromJson(Map<String, dynamic> json) => _$_EventDTO(
+EventDTO _$EventDTOFromJson(Map<String, dynamic> json) => EventDTO(
       json['id'] as int,
-      json['category_id'] as int,
-      json['date'] as String,
+      EventDTO._categoryById(json['category_id'] as int),
+      DateTime.parse(json['date'] as String),
       json['note'] as String?,
       json['audio'] as String?,
       json['short_desc'] as String?,
-      json['is_draft'] as int,
-      json['is_archive'] as int,
+      EventDTO._boolFromInt(json['is_draft'] as int),
+      EventDTO._boolFromInt(json['is_archive'] as int),
       json['music_group_id'] as int?,
       json['video'] as String?,
     );
-
-Map<String, dynamic> _$$_EventDTOToJson(_$_EventDTO instance) =>
-    <String, dynamic>{
-      'id': instance.id,
-      'category_id': instance.categoryId,
-      'date': instance.date,
-      'note': instance.note,
-      'audio': instance.audio,
-      'short_desc': instance.shortDesc,
-      'is_draft': instance.isDraft,
-      'is_archive': instance.isArchive,
-      'music_group_id': instance.musicGroupId,
-      'video': instance.video,
-    };
--- lib/src/data/remote/dto/event.dart
+++ lib/src/data/remote/dto/event.dart
@@ -1,29 +1,68 @@
 // ignore_for_file: invalid_annotation_target
 
-import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:json_annotation/json_annotation.dart';
 
-part '../../../../generated/src/data/remote/dto/event.freezed.dart';
 part '../../../../generated/src/data/remote/dto/event.g.dart';
 
-@freezed
-class EventDTO with _$EventDTO {
-  const EventDTO._();
+@JsonSerializable(createToJson: false)
+class EventDTO {
+  @JsonKey(name: 'id')
+  final int id;
+  @JsonKey(name: 'category_id', fromJson: _categoryById)
+  final CategoryDTO category;
+  @JsonKey(name: 'date')
+  final DateTime date;
+  @JsonKey(name: 'note')
+  final String? note;
+  @JsonKey(name: 'audio')
+  final String? audio;
+  @JsonKey(name: 'short_desc')
+  final String? shortDesc;
+  @JsonKey(name: 'is_draft', fromJson: _boolFromInt)
+  final bool isDraft;
+  @JsonKey(name: 'is_archive', fromJson: _boolFromInt)
+  final bool isArchive;
+  @JsonKey(name: 'music_group_id')
+  final int? musicGroupId;
+  @JsonKey(name: 'video')
+  final String? video;
 
-  const factory EventDTO(
-    @JsonKey(name: 'id') int id,
-    @JsonKey(name: 'category_id') int categoryId,
-    @JsonKey(name: 'date') String date,
-    @JsonKey(name: 'note') String? note,
-    @JsonKey(name: 'audio') String? audio,
-    @JsonKey(name: 'short_desc') String? shortDesc,
-    @JsonKey(name: 'is_draft') int isDraft,
-    @JsonKey(name: 'is_archive') int isArchive,
-    @JsonKey(name: 'music_group_id') int? musicGroupId,
-    @JsonKey(name: 'video') String? video,
-  ) = _EventDTO;
+  EventDTO(
+    this.id,
+    this.category,
+    this.date,
+    this.note,
+    this.audio,
+    this.shortDesc,
+    this.isDraft,
+    this.isArchive,
+    this.musicGroupId,
+    this.video,
+  );
 
   factory EventDTO.fromJson(Map<String, dynamic> json) =>
       _$EventDTOFromJson(json);
 
   static const fromJsonFactory = _$EventDTOFromJson;
+
+  static bool _boolFromInt(int value) => value > 0;
+
+  static CategoryDTO _categoryById(int id) {
+    switch (id) {
+      case 10:
+        return CategoryDTO.WORSHIP;
+      default:
+        return CategoryDTO.OTHER;
+    }
+  }
+
+  @override
+  String toString() {
+    return 'EventDTO{id: $id, category: $category, date: $date, note: $note, audio: $audio, shortDesc: $shortDesc, isDraft: $isDraft, isArchive: $isArchive, musicGroupId: $musicGroupId, video: $video}';
+  }
+}
+
+enum CategoryDTO {
+  WORSHIP,
+  OTHER,
 }
